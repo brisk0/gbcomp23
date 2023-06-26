f171fc59320d6a14738ee2db23141f3f05290408
brisk <bskblake@gmail.com>
2023-06-26 21:01:54 +0930

    Use MapInfo structs instead of macros
    The macros probably use less space and might have a performance benefit,
    but  the structs are just *so much cleaner*

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdbool.h>
 #include <gb/gb.h>
 #include <gb/cgb.h>
-#include "SPLASH.h"
+#include "assets/gbcompo23_splash.h"
 
 int main() {
-    set_bkg_palette(0, SPLASH_PALETTE_COUNT, SPLASH_palettes);
-    set_bkg_data(0, SPLASH_TILE_COUNT, SPLASH_tiles);
+    set_bkg_palette(0, gbcompo23_splash.tiles->num_pals, (uint16_t*)gbcompo23_splash.tiles->pals);
+    set_bkg_data(0, gbcompo23_splash.tiles->num_frames, gbcompo23_splash.tiles->data);
     VBK_REG = VBK_TILES;
-    set_bkg_tiles(0, 0, SPLASH_MAP_ATTRIBUTES_WIDTH, SPLASH_MAP_ATTRIBUTES_HEIGHT, SPLASH_map);
+    set_bkg_tiles(0, 0, gbcompo23_splash.width, gbcompo23_splash.height, gbcompo23_splash.data);
     VBK_REG = VBK_ATTRIBUTES;
-    set_bkg_tiles(0, 0, SPLASH_MAP_ATTRIBUTES_WIDTH, SPLASH_MAP_ATTRIBUTES_HEIGHT, SPLASH_map_attributes);
+    set_bkg_tiles(0, 0, gbcompo23_splash.width, gbcompo23_splash.height, gbcompo23_splash.attributes);
     SHOW_BKG;
     enable_interrupts();
     DISPLAY_ON;
